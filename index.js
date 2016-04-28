6df4f2e6903181c5f5d491c74cc1a97d3636032c
Tom Macie <tom@appuri.com>
2016-04-28 21:38:28 +0000

    handle the errors for all streams

--- index.js
+++ index.js
@@ -4,49 +4,58 @@ const ndjson = require('ndjson'),
       request = require('request'),
       zlib = require('zlib')
 
-module.exports = (readableObjectStream, eventWriteKey, eventSinkHost) => {
+function handleResponse(response, reject) {
 
-  const options = {
+  if (response.statusCode >= 400) {
+
+    var body = ''
+    response.setEncoding('utf8')
+    response.on('data', d => body += d)
+    response.on('error', () => reject({ statusCode: response.statusCode }))
+    response.on('end', () => {
+
+      try { body = JSON.parse(body) } catch(e){}
+      reject({
+        statusCode: response.statusCode,
+        responseBody: body
+      })
+    })
+  }
+}
+
+function promiseFromStreams(streams) {
+
+  return new Promise((resolve, reject) => {
+
+    streams.forEach((stream, i) => {
+
+      stream.on('error', reject)
+
+      if(i === streams.length - 1) {
+
+        let resp
+        stream.on('response', response => { resp = response; handleResponse(response, reject) })
+        stream.on('end', () => resolve(resp))
+      } else {
+        stream.pipe(streams[i + 1])
+      }
+    })
+  })
+}
+
+module.exports = (streams, eventWriteKey, eventSinkHost) => {
+
+  if(!Array.isArray(streams)) { streams = [streams] }
+
+  const serialize = ndjson.serialize(),
+        compress = zlib.createGzip(),
+        post = request.post({
           url: `https://${eventSinkHost || 'event-sink.appuri.net'}/e?jsonConnectorApiKey=${eventWriteKey}`,
           headers: {
             'Content-Type': 'application/x-ldjson',
             'Content-Encoding': 'gzip'
           }
-        },
-        serialize = ndjson.serialize(),
-        compress = zlib.createGzip(),
-        post = request.post(options)
-
-  readableObjectStream
-    .pipe(serialize)
-    .pipe(compress)
-    .pipe(post)
-
-  return new Promise((resolve, reject) => {
-
-    let resp
-
-    readableObjectStream.on('error', reject)
-    serialize.on('error', reject)
-    compress.on('error', reject)
-    post.on('error', reject)
-    post.on('response', response => {
-      resp = response
-      if (response.statusCode >= 400) {
-        var body = ''
-        response.setEncoding('utf8')
-        response.on('data', d => body += d)
-        response.on('error', () => reject({ statusCode: response.statusCode }))
-        response.on('end', () => {
-          try { body = JSON.parse(body) } catch(e){}
-          reject({
-            statusCode: response.statusCode,
-            responseBody: body
-          })
         })
-      }
-    })
 
-    post.on('end', () => resolve(resp))
-  })
+  return promiseFromStreams(streams.concat([serialize, compress, post]))
 }
